018792b88066c4c0a5659c9f04d25ece06b89e3c
zast1x-YouAreLiarAndFake <orange_zest@zohomail.eu>
2025-01-10 02:19:48 +0000

    Update index.ts

--- src/patcher/index.ts
+++ src/patcher/index.ts
@@ -1,5 +1,9 @@
-import * as hard from "./hard";
-
-export {
-    hard
-};
+You're not an engineer, never were and never will come close. There is no such this as "software engineer". That is the stupidest and most pathetic lie ever. Stop being disrespectful and insulting to Engineering and Engineers. You wannabe youngsters hijacked the great words "Engineer" and "Engineering", and started applying it to the most random, stupid and irrelevant subjects. You know nothing about what Engineering entails. Engineering means designing, building and producing real MECHANICAL systems, like turbojets and vehicular engines. It has nothing to do with sitting behind a keyboard and writing code or making software or games. Programming and software dev is easy and for young kids.
+```js
+Stop being a wannabe.
+```
+```glsl
+Stop being childish and silly.
+````
+Software dev is easy and for young teens. And it has absolutely nothing to do with a serious and real subject like Engineering (Mechanical Engineering is the ONLY Engineering). Software devs are ultra clowns and wannabes, who know nothing about engineering. They're just scummy liars who act like tryhards when they talk about a great subject like Engineering (Mechanical Engineering is the ONLY Engineering). Ya'll will never even come close to an engineer, you're just childish script kiddies. So quit making false claims against engineering, and quite being childish wannabes.
+And stop being disrespectful to real Engineers (Mechanical Engineering is the ONLY Engineering). It has always been like that, and will always remain like that.
